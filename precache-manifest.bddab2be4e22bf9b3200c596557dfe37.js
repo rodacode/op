6015670715687abb5e3b6a31ccaadf2b49d70aba
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-12-09 15:16:03 +0100

    Updates

---
+++ precache-manifest.bddab2be4e22bf9b3200c596557dfe37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc1b46b5403f82ddb15e73b63c89d15",
+    "url": "/op/index.html"
+  },
+  {
+    "revision": "2794ac93e5b21247a2e8",
+    "url": "/op/static/css/main.2bb35782.chunk.css"
+  },
+  {
+    "revision": "4a0c0e839b854cb51535",
+    "url": "/op/static/js/2.23a98f87.chunk.js"
+  },
+  {
+    "revision": "2794ac93e5b21247a2e8",
+    "url": "/op/static/js/main.414e605d.chunk.js"
+  },
+  {
+    "revision": "81500c9078e107b5ad1a",
+    "url": "/op/static/js/runtime-main.c7da3609.js"
+  },
+  {
+    "revision": "5b0453a7a2798c519d831b8add3465aa",
+    "url": "/op/static/media/clack.5b0453a7.wav"
+  },
+  {
+    "revision": "192a7dd387ba9c2be42274a88d9f019e",
+    "url": "/op/static/media/hh.192a7dd3.wav"
+  },
+  {
+    "revision": "d4272ef2c903e7c17b9bd6810b671fbf",
+    "url": "/op/static/media/hh2.d4272ef2.wav"
+  },
+  {
+    "revision": "f5717919192637ba88a8e69df851fabb",
+    "url": "/op/static/media/kick.f5717919.wav"
+  },
+  {
+    "revision": "507419ab439cf7a411137b83a85b1a93",
+    "url": "/op/static/media/perc.507419ab.wav"
+  }
+]);
