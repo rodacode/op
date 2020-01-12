0ef865e9632db8ad62a4871b3b0bd4d88a02ba56
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2020-01-12 21:54:27 +0100

    synth dev started

---
+++ src/components/synth/Synth.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import Nexus from "nexusui";
+import { Sequencer } from 'react-nexusui';
+import Tone from 'tone';
+
+import './synth.scss';
+
+const Synth = () => {
+    const isPlaying = useSelector(state => state.settings.isPlaying)
+    const bpm = useSelector(state => state.settings.bpm);
+    const matrix = useSelector(state => state.drumMachine.matrix)
+    const sequencerRef = useRef(Nexus.Sequencer);
+    const osc = new Tone.Oscillator(440, 'sine').toMaster();
+
+    const checkSound = (v) => {
+        osc.start()
+        console.log(v)
+        osc.stop()
+
+    }
+    const savePattern = (e) => {
+        e.preventDefault()
+        // dispatch({ type: 'SET_DRUMMACHINE_MATRIX', payload: sequencerRef.current.matrix.pattern })
+        // dispatch({ type: 'SET_DRUMMACHINE_CHANNELS_VOLUMEN', payload: [sound1.volume(), sound2.volume(), sound3.volume(), sound4.volume(), sound5.volume()] })
+        console.log('PATTERN SAVED!')
+    }
+
+    //PLAYING
+    useEffect(() => {
+        if (isPlaying) {
+            sequencerRef.current.start(bpm);
+        }
+        else {
+            sequencerRef.current.on = false;
+        }
+    }, [isPlaying, bpm]);
+
+    //POPULATE SEQUENCER MATRIX PATTERN
+    useEffect(() => {
+        sequencerRef.current.matrix.set.all(matrix);
+
+        //COLORS
+        sequencerRef.current.colorize("accent", "#ff0000");
+
+    });
+    return (
+        <div className="synth__main__container">
+            <div className="synth__header">
+                <h1>Synth</h1>
+                <button className="btn" onClick={savePattern}>Save Pattern</button>
+            </div>
+            <div className="drumMachine__container">
+                <div id="sequencer">
+                    <Sequencer
+                        rows={5}
+                        columns={16}
+                        size={[500, 300]}
+                        onStep={checkSound}
+                        onReady={sequencer => (sequencerRef.current = sequencer)}
+                    />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Synth;
--- src/containers/audioRack/AudioRack.js
+++ src/containers/audioRack/AudioRack.js
@@ -3,6 +3,7 @@ import React from 'react';
 import MainControls from '../../components/mainControls/MainControls';
 import DrumMachine from '../../components/drumMachine/DrumMachine';
 import Header from '../../components/header/Header';
+import Synth from '../../components/synth/Synth';
 import './audioRack.scss';
 
 const AudioRack = () => {
@@ -21,6 +22,7 @@ const AudioRack = () => {
                 <Header />
                 <MainControls />
                 <DrumMachine />
+                <Synth />
             </div>}
         </div>
 
