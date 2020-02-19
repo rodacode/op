625682a2461680132160e57301c0db796c98f399
Nicolás Hardmeier <nhardmeier@PFO-R90QAR9Q-LAP.pasiona.es>
2020-02-19 18:23:30 +0100

    added tests

--- src/__tests__/DrumMachine.test.js
+++ src/__tests__/DrumMachine.test.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, getByTestId } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import DrumMachine from '../components/drumMachine/DrumMachine';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
-import { initialDrumMachineState, drumMachineReducer } from './store/reducers/rootReducer'
-import Nexus from "nexusui";
 
 
-afterEach(cleanup);
 
-function renderWithRedux(
-    ui,
-    { initialState, store = createStore(drumMachineReducer, initialDrumMachineState) } = {}
-  ) {
-    return {
-      ...render(<Provider store={store}>{ui}</Provider>),
-      // adding `store` to the returned utilities to allow us
-      // to reference it in our tests (just try to avoid using
-      // this to test implementation details).
-      store,
-    }
-  }
+const store = createStore(() => ({ count: 1000 }))
+
 test('DrumMachine to be mounted', () => {
-    const { getByTestId } = renderWithRedux(<DrumMachine />);
-    // const DrumMachine = getByTestId('drumMachine');
+    const { getByTestId } = render(<Provider store={store}><DrumMachine /></Provider>);
 
 
-    expect(DrumMachine.textContent).toBe('Drum Machine')
+    expect(getByTestId('drumMachine')).toBe('Drum Machine')
 
 })
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux'
 
 const store = createStore(() => ({ count: 1000 }))
 
-describe.only("[Header Tests]", () => {
-  it.only(`should have to have an H1`, () => {
+describe("[Header Tests]", () => {
+  it(`should have to have an H1`, () => {
     const { getByTestId } = render(<Provider store={store}><Header /></Provider>);
 
     expect(getByTestId("headerH1")).toHaveTextContent("ONLINE PRODUCER");
