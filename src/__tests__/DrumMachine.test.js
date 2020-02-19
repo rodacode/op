687db07f9c85c6179cea8e4b8d2949fdda423fbc
Nicolás Hardmeier <nhardmeier@PFO-R90QAR9Q-LAP.pasiona.es>
2020-02-19 18:25:07 +0100

    added tests2

--- src/__tests__/DrumMachine.test.js
+++ src/__tests__/DrumMachine.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getByTestId } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import DrumMachine from '../components/drumMachine/DrumMachine';
 import { createStore } from 'redux';
