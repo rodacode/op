c567094c6b87cb3d61dbce7569c15730f851e849
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2020-02-22 00:38:04 +0100

    added test

--- src/__tests__/DrumMachine.test.js
+++ src/__tests__/DrumMachine.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import DrumMachine from '../components/drumMachine/DrumMachine';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import { initialDrumMachineState, drumMachineReducer } from './store/reducers/rootReducer'
 import Nexus from "nexusui";
@@ -10,21 +9,11 @@ import Nexus from "nexusui";
 
 afterEach(cleanup);
 
-function renderWithRedux(
-    ui,
-    { initialState, store = createStore(drumMachineReducer, initialDrumMachineState) } = {}
-  ) {
-    return {
-      ...render(<Provider store={store}>{ui}</Provider>),
-      // adding `store` to the returned utilities to allow us
-      // to reference it in our tests (just try to avoid using
-      // this to test implementation details).
-      store,
-    }
-  }
+const store = () => null;
+
 test('DrumMachine to be mounted', () => {
-    const { getByTestId } = renderWithRedux(<DrumMachine />);
-    // const DrumMachine = getByTestId('drumMachine');
+    const { getByTestId } = render(<Provider store={store}><DrumMachine /></Provider>);
+    const DrumMachine = getByTestId('drumMachine');
 
 
     expect(DrumMachine.textContent).toBe('Drum Machine')
